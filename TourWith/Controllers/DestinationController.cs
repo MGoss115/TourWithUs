54c2284e8f521a552cc132ea9367ed034c609115
mligon115 <mligon115@yahoo.com>
2023-03-10 17:32:04 -0600

    saving

--- TourWith/Controllers/DestinationController.cs
+++ TourWith/Controllers/DestinationController.cs
@@ -30,31 +30,6 @@ public class DestinationController : Controller
         }
     }
 
-
-    // [HttpGet("destination")]
-    // public async Task<IActionResult> Index()
-    // {
-    //     var authKey = Convert.ToBase64String(Encoding.UTF8.GetBytes("a63fdddd565ec353f2451ac96b54cb46:f1c8f86af1c3050149f1e4bef5b4e5a3"));
-
-    //     var request = new HttpRequestMessage(HttpMethod.Get, "https://api.roadgoat.com/api/v2/destinations/new-york-ny-usa");
-    //     request.Headers.Add("Authorization", $"Basic {authKey}");
-
-    //     var response = await _httpClient.SendAsync(request);
-
-    //     if (!response.IsSuccessStatusCode)
-    //     {
-    //         return StatusCode((int)response.StatusCode);
-    //     }
-
-    //     var content = await response.Content.ReadAsStringAsync();
-    //     var result = JsonConvert.DeserializeObject<dynamic>(content);
-    //     Console.WriteLine($"here is the {result}");
-
-
-    //     return View();
-
-    // }
-
     [SessionCheck]
     [HttpGet("destination")]
     public async Task<IActionResult> Index()
